355786c51fa8df75a062af9001dc5ccd2e8e946b
Poya Kazemzade <poykaz0506@skola.goteborg.se>
2023-11-21 00:42:09 +0100

    modified instance of guesser in GuesserGame.java

--- GuesserGame.java
+++ GuesserGame.java
@@ -1,6 +1,6 @@
 public class GuesserGame {
     public static void main(String[] args) {
-        Guesser guesser = new Guesser(0, 1000);
+        Guesser guesser = new Guesser(0, 10);
         guesser.start();
     }
 }
